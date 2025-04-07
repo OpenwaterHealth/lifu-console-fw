a35d4c84f535cf32883275d06d0c1b1b02a3ef59
George Vigelette <george@openwater.cc>
2025-04-07 14:27:25 -0400

    moved duplicate function

--- Core/Inc/hv_supply.h
+++ Core/Inc/hv_supply.h
@@ -34,7 +34,6 @@ uint16_t HV_GetVoltage();
 uint16_t HV_GetOnVoltage();
 void set_use_exact(bool val);
 void HV_Enable(void);
-void HV_Enable_Exact(void);
 void HV_Disable(void);
 void HV_ClearDAC(void);
 void V12_Enable(void);
